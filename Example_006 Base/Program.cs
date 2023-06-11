f68359cb88c003bc77ef7929ee4ccb196a9e5601
Nikolay Karasev <nomotorolla@gmail.com>
2023-06-12 01:40:45 +0300

    add new folder Examplle_007 Magic

--- Example_006 Base/Program.cs
+++ Example_006 Base/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 4;
+int b = 3;
+int c = 12;
+int d = 1;
+int e = 7;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write ("max = ");
+Console.WriteLine (max);
+
