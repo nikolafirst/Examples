ffa6f6303ae1e51e5533caabbe927ca843765ffc
Nikolay Karasev <nomotorolla@gmail.com>
2023-06-11 23:55:17 +0300

    add random

--- Example_003 Summa/Program.cs
+++ Example_003 Summa/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 33;
-int numberB = 7;
+﻿int numberA = new Random().Next(1,10); // 0,1,2,....,9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
